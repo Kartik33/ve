f56f0052562a81f19f78af8cbebefdc20d85eb2f
Kartik <kartiktushir33@gmail.com>
2019-12-02 12:32:48 -0600

    ve

---
+++ classes.py
@@ -0,0 +1,15 @@
+class table:
+    def __init__(self, name):
+        self.name = name
+        
+    table = None
+    headings = None
+    domain = None
+    
+class headings:
+    def __init__(self, name):
+        self.name = name
+        
+    linked_tables = None
+    headings = None
+    domain = None
---
+++ init_table.py
@@ -0,0 +1,20 @@
+from classes import *
+import numpy as np
+
+t1 = table("ABC")
+t1.table = np.random.randint((2,2,2))
+t1.headings = ["A","B","C"]
+
+t2 = table("BC")
+t2.table = np.random.randint((2,2))
+t2.headings = ["B","C"]
+
+               
+t3 = table("AB")
+t3.table = np.random.randint((2,2))
+t3.headings = ["A","B"]
+
+
+t4 = table("CD")
+t4.table = np.random.randint((2,2,2))
+t4.headings = ["D", "C"]
